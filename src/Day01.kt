1d4affefe2e4e4a61d7df07e32a3e68fb71da5ba
dkoval <koval.dmitriy@gmail.com>
2021-12-01 09:39:58 +0100

    Solutions to Day01 problem

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val nums = input.map { it.toInt() }
+        var count = 0
+        for (i in 1 until nums.size) {
+            if (nums[i] > nums[i - 1]) {
+                count++
+            }
+        }
+        // answer = 1448
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val nums = input.map { it.toInt() }
+        var count = 0
+        // we know, there are >= 3 numbers in Day01 input
+        var prevSum = nums.subList(0, 3).sum()
+        for (i in 1..nums.size - 3) {
+            val currSum = prevSum - nums[i - 1] + nums[i + 2]
+            if (currSum > prevSum) {
+                count++
+            }
+            prevSum = currSum
+        }
+        // answer = 1471
+        return count
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
