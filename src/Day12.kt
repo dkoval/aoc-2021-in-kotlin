5cabe16fcc81dfa075ba1ec71824639fb8cb45f6
dkoval <koval.dmitriy@gmail.com>
2021-12-12 21:08:21 +0100

    Reduce memory footprint by only computing the number of distinct paths from source to destination rather than generating them

--- src/Day12.kt
+++ src/Day12.kt
@@ -12,97 +12,85 @@ fun main() {
         return adj
     }
 
-    fun dfs1(
-        adj: Map<String, List<String>>,
-        u: String,
-        target: String,
-        visited: MutableSet<String>,
-        currPath: MutableList<String>,
-        distinctPaths: MutableList<List<String>>
-    ) {
-        if (u == target) {
-            val copy = currPath.toMutableList()
-            copy += target
-            distinctPaths += copy
-            return
-        }
-
-        if (u in visited) {
-            return
-        }
-
-        // mark the current vertex as visited
-        if (u[0].isLowerCase()) {
-            visited += u
-        }
-
-        // run DFS for vertices adjacent to u
-        currPath += u
-        for (v in adj[u]!!) {
-            dfs1(adj, v, target, visited, currPath, distinctPaths)
-        }
-
-        // backtrack to find more paths
-        if (u[0].isLowerCase()) {
-            visited -= u
-        }
-        currPath.removeAt(currPath.lastIndex)
-    }
-
     fun part1(input: List<String>): Int {
-        val adj = adj(input)
-        val distinctPaths = mutableListOf<List<String>>()
-        dfs1(adj, start, end, mutableSetOf(), mutableListOf(), distinctPaths)
-        return distinctPaths.size
-    }
-
-    fun dfs2(
-        adj: Map<String, List<String>>,
-        u: String,
-        target: String,
-        visited: MutableMap<String, Int>,
-        currPath: MutableList<String>,
-        distinctPaths: MutableList<List<String>>
-    ) {
-        if (u == target) {
-            val copy = currPath.toMutableList()
-            copy += target
-            distinctPaths += copy
-            return
-        }
-
-        if (u in visited && (u == start || visited[u] == 2 || currPath.any { x -> visited[x] == 2 })) {
-            return
-        }
-
-        // mark the current vertex as visited
-        if (u[0].isLowerCase()) {
-            visited[u] = (visited[u] ?: 0) + 1
-        }
-
-        // run DFS for vertices adjacent to u
-        currPath += u
-        for (v in adj[u]!!) {
-            dfs2(adj, v, target, visited, currPath, distinctPaths)
-        }
-
-        // backtrack to find more paths
-        if (u[0].isLowerCase()) {
-            val count = visited[u]!!
-            if (count == 1) {
-                visited -= u
-            } else {
-                visited[u] = count - 1
+        fun dfs1(
+            adj: Map<String, List<String>>,
+            u: String,
+            target: String,
+            visited: MutableSet<String>
+        ): Int {
+            if (u == target) {
+                return 1
             }
+
+            if (u in visited) {
+                return 0
+            }
+
+            // mark the current vertex as visited
+            val smallCave = u[0].isLowerCase()
+            if (smallCave) {
+                visited += u
+            }
+
+            // having "appended" vertex u to the current path, run DFS for vertices adjacent to u
+            var count = 0
+            for (v in adj[u]!!) {
+                count += dfs1(adj, v, target, visited)
+            }
+
+            // backtrack to find more paths
+            if (smallCave) {
+                visited -= u
+            }
+            return count
         }
-        currPath.removeAt(currPath.lastIndex)
+
+        val adj = adj(input)
+        return dfs1(adj, start, end, mutableSetOf())
     }
 
     fun part2(input: List<String>): Int {
+        fun dfs2(
+            adj: Map<String, List<String>>,
+            u: String,
+            target: String,
+            visited: MutableMap<String, Int>,
+        ): Int {
+            if (u == target) {
+                return 1
+            }
+
+            if (u in visited && (u == start || visited[u] == 2 || visited.any { (_, times) -> times == 2 })) {
+                return 0
+            }
+
+            // mark the current vertex as visited
+            val smallCave = u[0].isLowerCase()
+            if (smallCave) {
+                visited[u] = (visited[u] ?: 0) + 1
+            }
+
+            // having "appended" vertex u to the current path, run DFS for vertices adjacent to u
+            var count = 0
+            for (v in adj[u]!!) {
+                count += dfs2(adj, v, target, visited)
+            }
+
+            // backtrack to find more paths
+            if (smallCave) {
+                val times = visited[u]!!
+                if (times > 1) {
+                    visited[u] = times - 1
+                } else {
+                    visited -= u
+                }
+            }
+            return count
+        }
+
         val adj = adj(input)
-        val distinctPaths = mutableListOf<List<String>>()
-        dfs2(adj, start, end, mutableMapOf(), mutableListOf(), distinctPaths)
-        return distinctPaths.size
+        return dfs2(adj, start, end, mutableMapOf())
     }
 
     check(part1(readInput("Day12_test1")) == 10)
