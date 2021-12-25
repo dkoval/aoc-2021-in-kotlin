ead390b73ec75aac05796970a97780f6e195731c
dkoval <koval.dmitriy@gmail.com>
2021-12-25 10:26:23 +0100

    Code readability improvements

--- src/Day25.kt
+++ src/Day25.kt
@@ -1,41 +1,45 @@
+enum class Herd(val marker: Char) {
+    EAST_FACING('>'),
+    SOUTH_FACING('v')
+}
+
 fun main() {
-    fun Array<CharArray>.copy(): Array<CharArray> = map { it.copyOf() }.toTypedArray()
-
     fun part1(input: List<String>): Int {
-        var grid = input.map { it.toCharArray() }.toTypedArray()
-        val numRows = grid.size
-        val numCols = grid[0].size
+        fun moveHerd(grid: Array<CharArray>, herd: Herd): Pair<Array<CharArray>, Boolean> {
+            val numRows = grid.size
+            val numCols = grid[0].size
 
+            var moved = false
+            val copy = grid.map { it.copyOf() }.toTypedArray()
+            for (row in 0 until numRows) {
+                for (col in 0 until numCols) {
+                    if (grid[row][col] == herd.marker) {
+                        // can a sea cucumber move further?
+                        val (nextRow, nextCol) = when (herd) {
+                            Herd.EAST_FACING -> row to (col + 1) % numCols
+                            Herd.SOUTH_FACING -> (row + 1) % numRows to col
+                        }
+                        if (grid[nextRow][nextCol] == '.') {
+                            copy[row][col] = '.'
+                            copy[nextRow][nextCol] = herd.marker
+                            moved = true
+                        }
+                    }
+                }
+            }
+            return copy to moved
+        }
+
+        // simulate moves
+        var grid = input.map { it.toCharArray() }.toTypedArray()
         var count = 1
         var moving = true
         while (moving) {
-            moving = false
+            val (gridAfterEastFacingHerdMove, eastFacingHerdMoved) = moveHerd(grid, Herd.EAST_FACING)
+            val (gridAfterSouthFacingHerdMove, southFacingHerdMoved) = moveHerd(gridAfterEastFacingHerdMove, Herd.SOUTH_FACING)
 
-            // state of the grid after the east-facing herd moved
-            val grid2 = grid.copy()
-            for (row in 0 until numRows) {
-                for (col in 0 until numCols) {
-                    if (grid[row][col] == '>' && grid[row][(col + 1) % numCols] == '.') {
-                        grid2[row][col] = '.'
-                        grid2[row][(col + 1) % numCols] = '>'
-                        moving = true
-                    }
-                }
-            }
-
-            // state of the grid after the south-facing herd moved
-            val grid3 = grid2.copy()
-            for (row in 0 until numRows) {
-                for (col in 0 until numCols) {
-                    if (grid2[row][col] == 'v' && grid2[(row + 1) % numRows][col] == '.') {
-                        grid3[row][col] = '.'
-                        grid3[(row + 1) % numRows][col] = 'v'
-                        moving = true
-                    }
-                }
-            }
-
-            grid = grid3
+            grid = gridAfterSouthFacingHerdMove
+            moving = eastFacingHerdMoved || southFacingHerdMoved
             if (moving) {
                 count++
             }
